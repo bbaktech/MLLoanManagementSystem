b75e7c4b8628e2caf3e84f10b0a0c3d5cc80a7b7
BBAK <bbaktech@gmail.com>
2026-04-27 19:44:19 +0530

    run bat updated for jar

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-javaw -classpath "C:\eclipseworkspace\LakshmiBankLoanSystem\bin;C:\ML_LOAN_PROJECT\mysql-connector-j-8.0.33.jar;C:\ML_LOAN_PROJECT\protobuf-java-3.21.9.jar" NewOkCancelDialog
+javaw -classpath "C:\new_eclipse_workspace\MLSystem\bin;mysql-connector-j-8.0.33.jar;protobuf-java-3.21.9.jar;pdfbox-3.0.5.jar;itextpdf-5.5.13.4.jar" NewOkCancelDialog
